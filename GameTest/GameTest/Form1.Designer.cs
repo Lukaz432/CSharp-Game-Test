0a4efdcdacb8ff9b74533fc7107dad8b565a3e1c
Lukaz432 <55126350+Lukaz432@users.noreply.github.com>
2020-06-26 18:37:01 +0300

    Creation-of-multiple-objects

---
+++ GameTest/GameTest/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace GameTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.creatObjectsBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // creatObjectsBtn
+            // 
+            this.creatObjectsBtn.Location = new System.Drawing.Point(112, 319);
+            this.creatObjectsBtn.Name = "creatObjectsBtn";
+            this.creatObjectsBtn.Size = new System.Drawing.Size(133, 60);
+            this.creatObjectsBtn.TabIndex = 0;
+            this.creatObjectsBtn.Text = "Create Objects";
+            this.creatObjectsBtn.UseVisualStyleBackColor = true;
+            this.creatObjectsBtn.Click += new System.EventHandler(this.creatObjectsBtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(112, 32);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(454, 250);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 422);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.creatObjectsBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button creatObjectsBtn;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ GameTest/GameTest/Form1.cs
@@ -0,0 +1,46 @@
+﻿using GameClasses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void creatObjectsBtn_Click(object sender, EventArgs e)
+        {
+            Thing th1;
+            Thing th2;
+            Treasure tr1;
+            Treasure tr2;
+            Room r1;
+            Room r2;
+
+            th1 = new Thing("A broom: ", "Looks like a broom");
+            th2 = new Thing("A shadow  ", "Thats a weird looking shadow");
+            tr1 = new Treasure("Writ's leg: ", "Wonder where's Writ", 100);
+            tr2 = new Treasure("Ring of recoil: ", "Don't break it!", 250.50);
+            r1 = new Room("Dark Room: ", "A room that nobody shall enter", 1, 4, 7, 8);
+            r2 = new Room("Rabbit Hole: ", "A hole where the evil bunny resides", 1, 4, 6, 3);
+
+            textBox1.Text = "Here we go!\r\n" +
+                th1.Name + th1.Description + "\r\n" +
+                th2.Name + th2.Description + "\r\n" +
+                tr1.Name + tr1.Description + " which is worth Gr" + tr1.Value + "\r\n" +
+                tr2.Name + tr2.Description + " which is worth Gr" + tr2.Value + "\r\n" +
+                "You can see the " + r1.Name + r1.Description + " with exits to " + r1.N + ", " + r1.S + ", " + r1.W + " and " + r1.E + " and " +
+                r2.Name + r2.Description + " with exits to " + r2.N + ", " + r2.S + ", " + r2.W + " and " + r2.E;
+        }
+    }
+}
